35528d4d28ef6f7ffeb2fab2c43aa6c55f0f427f
Shrawan Choudhary <shrawan07090@gmail.com>
2020-10-12 22:06:15 +0530

    design price template
    Signed-off-by: Shrawan Choudhary <shrawan07090@gmail.com>

--- strangeflix/screens/urls.py
+++ strangeflix/screens/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import (
-    HomeView , WelcomeScreen
+    HomeView, WelcomeScreen,
+    PriceView,
 )
 
 app_name = "screens"
@@ -9,5 +10,6 @@ urlpatterns = [
     #path('', welcome_screen,name='home'),
     path('', WelcomeScreen.as_view(), name="welcome"),
     path('loggedin/', HomeView.as_view(), name="loggedin"),
+    path('price-list/', PriceView.as_view(), name="price"),
     #path('/accounts', include('allauth.urls')),
 ]
--- strangeflix/screens/views.py
+++ strangeflix/screens/views.py
@@ -4,18 +4,18 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Movie
 from django.views.generic import TemplateView, ListView, DeleteView
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(login_required, name='dispatch')
 class HomeView(ListView):
     model = Movie
     template_name = "home.html"
 
-def home(request):
-    context = {
-        'items': Movie.objects.all()
-    }
-    return render(request, "home.html", context)
-
 
 class WelcomeScreen(TemplateView):
     template_name = "welcome.html"
+
+
+class PriceView(TemplateView):
+    template_name = "price.html"
